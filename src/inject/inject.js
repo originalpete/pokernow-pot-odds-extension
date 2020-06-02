9c32e56c00e1b40445d8fbc13f442479fd330497
Peter MacRobert <originalpete@gmail.com>
2020-06-02 15:09:45 -0400

    forcing a recalc after flop, turn and river

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,15 +8,46 @@ var readyStateCheckInterval = setInterval(function() {
   }
 }, 10);
 
-// Set up listener port to the background extension script
-var port = chrome.runtime.connect({name: "background"});
-port.onMessage.addListener(function(msg) {
-  console.log(msg);
-  // if (msg.type == "wineResult") {
-  //   updateWine(msg);
-  // }
-});
+// Setup, build output UI, and attach DOM observers
+var doSetup = function() {
 
+  // inject pot odds UI box
+  var html = "<div class='pot-odds-container'>Pot odds:&nbsp;<span class='pot-odds-value'>&mdash;</span></div>";
+  jQuery(".table").prepend(jQuery(html));
+
+  // Initiate observers
+  var targetNode = jQuery(".table")[0];
+
+  // Options for the observer (which mutations to observe)
+  const config = {characterData: true, attributes: true, childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  var callback = function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+
+      // Skip non-element nodes
+      if (
+        !mutation.target.getAttribute
+        || !mutation.target.getAttribute('class')
+      ) { continue }
+
+      var c = mutation.target.getAttribute('class');
+
+      if ( c.match(/decision\-current|flipped/) ) 
+      { 
+        updatePotOdds();
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  var observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+
+  console.log("...setup done.")
+};
 
 // Do the main thing
 var updatePotOdds = function() {
@@ -61,41 +92,4 @@ var updatePotOdds = function() {
     jQuery(".pot-odds-value").html(potOdds);
   }
 
-};
-
-
-var doSetup = function() {
-
-  // inject pot odds UI box
-  var html = "<div class='pot-odds-container'>Pot odds:&nbsp;<span class='pot-odds-value'>&mdash;</span></div>";
-  jQuery(".table").prepend(jQuery(html));
-
-  // Initiate observers
-  var targetNode = jQuery(".table")[0];
-
-  // Options for the observer (which mutations to observe)
-  const config = {characterData: true, attributes: true, childList: true, subtree: true };
-
-  // Callback function to execute when mutations are observed
-  var callback = function(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-      if (
-        mutation.target.getAttribute
-        && mutation.target.getAttribute('class') 
-        && mutation.target.getAttribute('class').match("decision-current")
-      ) {
-        updatePotOdds();
-      }
-    }
-  };
-
-  // Create an observer instance linked to the callback function
-  var observer = new MutationObserver(callback);
-
-  // Start observing the target node for configured mutations
-  observer.observe(targetNode, config);
-
-  port.postMessage({type: "foo", data: "bar"});
-
-  console.log("...setup done.")
 };
